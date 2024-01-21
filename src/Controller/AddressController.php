a1c7066dc64971c70f301e2d8a3438a591ee3eda
Robus <herrkolacki@gmail.com>
2024-01-21 17:16:18 +0100

    proba mikrofona '

--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -10,7 +10,8 @@ class AddressController extends AbstractController
 {
     #[Route('/address', name: 'app_address')]
     public function index(): Response
-    {  
+    {   
+        var_dump('to jest próba mikrofona');
         return $this->render('address/index.html.twig', [
             'controller_name' => 'AddressController',
         ]);
