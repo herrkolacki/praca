e29430c2a310cab06967f25f3a0e9a9c7c9286fa
Robus <herrkolacki@gmail.com>
2024-01-21 16:32:02 +0100

    to jest commit na main

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -16,32 +16,12 @@ class CompanyController extends AbstractController
     // #[Route('/company', name: 'app_company')]
     #[Route('/', name: 'homepage')]
     public function index(Request $request, Environment $twig, CompanyRepository $companyRepository): Response
-    {
-     
-       /* var_dump($companyRepository->skarb('(())*(((()))'));        
-         $orderItems[0]['ilosc'] = 0;
-       $orderItems[0]['cena_szt'] = 10;
-       $orderItems[0]['id_towaru'] = 1;
-       
-       $orderItems[1]['ilosc'] = 20;
-       $orderItems[1]['cena_szt'] = 20;
-       $orderItems[1]['id_towaru'] = 2;
+    {  
+        var_dump($companyRepository->findAllGreaterThanPrice('t'));
 
-       $orderItems[2]['ilosc'] = 30;
-       $orderItems[2]['cena_szt'] = 30;
-       $orderItems[2]['id_towaru'] = 3;
-
-       $orderItems[3]['ilosc'] = 40;
-       $orderItems[3]['cena_szt'] = 40;
-       $orderItems[3]['id_towaru'] = 4;
-
-        var_dump($companyRepository->calculateDeliveryCosts($orderItems, 10.10));
-        die();
-        print_r($companyRepository->boldWords('<p>cos</p> <p>nic</p> <p>wszy lawa</p> <b><p>zmo</p></b>', ['cos','nic', 'zmo', 'lawa']));*/
-        //companies czyta z eventSubscribers
-        return $this->render('company/index.html.twig', []);
-                       //    'companies' => $companyRepository->findAll(),
-                       //]);
+        return $this->render('company/index.html.twig', [
+                           'companies' => $companyRepository->findByExampleField('tpay', 4)
+                       ]);
         
 
 
--- src/Repository/CompanyRepository.php
+++ src/Repository/CompanyRepository.php
@@ -25,11 +25,43 @@ class CompanyRepository extends ServiceEntityRepository
         parent::__construct($registry, Company::class);
     }
 
+    public function findByExampleField(string $name, int $id): array
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.name = :val')
+            ->orWhere('c.id = :id')
+            ->setParameter('id', $id)
+            ->setParameter('val', $name)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+    }
+
+
+    public function findAllGreaterThanPrice(string $name): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+               SELECT * FROM company c LEFT JOIN position p ON c.id=p.company_id
+               WHERE c.name like :name
+               ORDER BY c.name ASC
+               ';
+               
+        $resultSet = $conn->executeQuery($sql, ['name' =>'%'.$name.'%']);
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $resultSet->fetchAllAssociative();
+    }
+
     public function getCommentPaginator(Company $company, int $offset): Paginator
     {
         $query = $this->createQueryBuilder('c')
-            ->andWhere('c.company = :company') 
+            ->andWhere('c.company = :company')
+            ->andWhere('c.id = :id')
             ->setParameter('company', $company)
+            ->setParameter('id', 3)
             ->orderBy('c.createdAt', 'DESC')
             ->setMaxResults(self::PAGINATOR_PER_PAGE)
             ->setFirstResult($offset)
@@ -42,7 +74,7 @@ class CompanyRepository extends ServiceEntityRepository
 
 
 
-   public function boldWords(string $html, array $allergens): string
+    public function boldWords(string $html, array $allergens): string
     {
         $items = explode(' ', $html);
         foreach ($items as &$item) {
@@ -55,49 +87,51 @@ class CompanyRepository extends ServiceEntityRepository
         }
         return implode(' ', $items);
     }
-    
+
     public function calculateDeliveryCosts(array $orderItems, float $deliveryCost): array
     {
-       foreach ($orderItems as &$orderItem) {
-           if(isset($orderItem['ilosc']) && $orderItem['ilosc'] > 0 && isset($orderItem['cena_szt'])) {
+        foreach ($orderItems as &$orderItem) {
+            if (isset($orderItem['ilosc']) && $orderItem['ilosc'] > 0 && isset($orderItem['cena_szt'])) {
                 $orderItem['cena_szt_z_kosztami_wysylki'] = round($deliveryCost / $orderItem['ilosc'], 2) + $orderItem['cena_szt'];
-           }
+            }
         }
         return $orderItems;
     }
 
-    public function kwerenda_raportu() {
+    public function kwerenda_raportu()
+    {
         $dateFrom = date('Y-m-d', $_POST['unix_timestamp_od']);
         $dateTo =  date('Y-m-d', $_POST['unix_timestamp_do']);
-        return '
-            SELECT COUNT(o.orders_id) orders_count, SUM(op.products_price * op.products_quantity) orders_val, osh.status_id as statusId
+        $listStatus = $_POST['lista_statusow'];
+        return "
+            SELECT COUNT(o.orders_id) orders_count, SUM(op.products_price * op.products_quantity) orders_val,
+             osh.status_id as statusId
             FROM orders o
             INNER JOIN orders_products op USING(orders_id)
-            INNER JOIN orders_status_history osh ON osh.orders_id = o.orders_id AND osh.status_id IN({$_POST[‘lista_statusow’]})
+            INNER JOIN orders_status_history osh ON osh.orders_id = o.orders_id AND osh.status_id IN({$listStatus})
             WHERE o.date_purchased BETWEEN {$dateFrom} AND {$dateTo} group by osh.status_id;
-        ';
+        ";
     }
 
     public function skarb(string $input)
     {
-            $inputArray = str_split($input);
-            $outputArray = [];
-            $level = 0;
-            $i = 0;
-        foreach($inputArray as  $iArr){
-               
-                if($iArr == '('){
-                    $level = $level -1;
-                   
-                }elseif($iArr == ')'){
-                    $level = $level +1;
-                }
+        $inputArray = str_split($input);
+        $outputArray = [];
+        $level = 0;
+        $i = 0;
+        foreach ($inputArray as  $iArr) {
 
-                $outputArray[$i]['index'] = $i;
-                $outputArray[$i]['ciag'] = $iArr;
-                $outputArray[$i]['poziom'] = $level;
-                $i++;
+            if ($iArr == '(') {
+                $level = $level - 1;
+            } elseif ($iArr == ')') {
+                $level = $level + 1;
             }
+
+            $outputArray[$i]['index'] = $i;
+            $outputArray[$i]['ciag'] = $iArr;
+            $outputArray[$i]['poziom'] = $level;
+            $i++;
+        }
         return $outputArray;
     }
 
